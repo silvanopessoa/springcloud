d130c86d5a1925855b5dec78ce72213bc423cb86
Silvano <silvano.pessoa@gmail.com>
2016-03-01 15:18:34 -0300

    Signed-off-by: Silvano <silvano.pessoa@gmail.com>

---
+++ src/main/java/br/com/silvanopessoa/SpringcloudApplication.java
@@ -0,0 +1,12 @@
+package br.com.silvanopessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudApplication.class, args);
+	}
+}
